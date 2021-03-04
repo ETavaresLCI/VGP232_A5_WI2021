d6179cce356a3b11b0c518dee02660df77c41c5b
Orion Roussopoulos <orion@orionpeace.com>
2021-03-03 16:56:59 -0800

    Fixed Charecter Stuff

--- Assignment5/Assignment5/Character.cs
+++ Assignment5/Assignment5/Character.cs
@@ -42,7 +42,12 @@ namespace Assignment5
         /// <param name="damage">The amount of damage taken</param>
         public void TakeDamage(int damage)
         {
-            Health = 100;
+            Health -= damage;
+            if (Health < 0)
+			{
+                IsAlive = false;
+                Health = 0;
+			}
         }
 
         /// <summary>
@@ -51,7 +56,11 @@ namespace Assignment5
         /// <param name="amount">The amount of health to recover</param>
         public void RestoreHealth(int amount)
         {
-            Health = 1;
+            Health += amount;
+            if (Health > MaxHealth)
+			{
+                Health = MaxHealth;
+			}
         }
 
         public override string ToString()
