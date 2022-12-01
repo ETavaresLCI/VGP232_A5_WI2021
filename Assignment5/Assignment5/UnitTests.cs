d302515fadcc530c649c1d9c612e959fbfd7d199
MarkOasan <markoasan@gmail.com>
2022-12-01 01:41:16 -0800

    Refactored codes Replace all writelines into Assert Added MaxSlots and AvailableSlots int variables for comparisons

--- Assignment5/Assignment5/UnitTests.cs
+++ Assignment5/Assignment5/UnitTests.cs
@@ -9,11 +9,13 @@ namespace Assignment5
     internal class UnitTests
     {
         private Inventory PlayerInventory;
-
+        private int MaxSlots = 5;
+        private int AvailableSlots;
         [SetUp]
         public void SetUp()
         {
-            PlayerInventory = new Inventory(5);
+            
+            PlayerInventory = new Inventory(MaxSlots);
             PlayerInventory.AddItem(new Item("Wooden Sword", 1, ItemGroup.Equipment));
         }
 
@@ -26,46 +28,36 @@ namespace Assignment5
         [Test]
         public void RemoveItemIsSet()
         {
-            Console.WriteLine($"Current Available slot: {PlayerInventory.AvailableSlots.ToString()}");
+            AvailableSlots = PlayerInventory.AvailableSlots;
             Assert.IsTrue(PlayerInventory.TakeItem("Wooden Sword", out Item found));
-            Console.WriteLine($"Found item: {found.Name.ToString()}");
-            Console.WriteLine($"Updated Available slot: {PlayerInventory.AvailableSlots.ToString()}");
+            Assert.IsNotNull(found);
+            Assert.Greater(PlayerInventory.AvailableSlots, AvailableSlots);
         }
 
         [Test]
         public void RemoveItemIsNull()
         {
-            Console.WriteLine($"Current Available slot: {PlayerInventory.AvailableSlots.ToString()}");
-            PlayerInventory.TakeItem("Apple", out Item found);
-            Assert.IsFalse(found !=null);
-            Console.WriteLine($"Updated Available slot: {PlayerInventory.AvailableSlots.ToString()}");
+            AvailableSlots = PlayerInventory.AvailableSlots;
+            Assert.IsFalse(PlayerInventory.TakeItem("Apple", out Item found));
+            Assert.IsNull(found);
+            Assert.AreEqual(PlayerInventory.AvailableSlots, AvailableSlots);
         }
 
         [Test]
         public void AddItem()
         {
-            Console.WriteLine($"Current Available slot: {PlayerInventory.AvailableSlots.ToString()}");
+            AvailableSlots = PlayerInventory.AvailableSlots;
             Item item = new Item("Blue Berry", 1, ItemGroup.Consumable);
-            Console.WriteLine($"Adding item: {item.Name} Amount: {item.Amount} Group: {item.Group.ToString()}");
-            PlayerInventory.AddItem(item);
-            foreach (Item listItem in PlayerInventory.ListAllItems())
-            {
-                Console.WriteLine(listItem.ToString());
-            }
-            Console.WriteLine($"Updated Available slot: {PlayerInventory.AvailableSlots.ToString()}");
+            Assert.IsTrue(PlayerInventory.AddItem(item));
+            Assert.IsTrue(PlayerInventory.ListAllItems().IndexOf(item)>0);
+            Assert.Less(PlayerInventory.AvailableSlots, AvailableSlots);
         }
 
         [Test]
         public void Reset()
         {
-            Console.WriteLine($"Current Available slot: {PlayerInventory.AvailableSlots.ToString()}");
             PlayerInventory.Reset();
-            Console.WriteLine($"Showing list of items: ");
-            foreach (Item listItem in PlayerInventory.ListAllItems())
-            {
-                Console.WriteLine(listItem.ToString());
-            }
-            Console.WriteLine($"Available slot: {PlayerInventory.AvailableSlots.ToString()}");
+            Assert.AreEqual(PlayerInventory.AvailableSlots, MaxSlots);
         }
     }
 }
